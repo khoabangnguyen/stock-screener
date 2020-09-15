ee50f55899ae0a0ccedadc62af5a61212b92027f
Khoa <khoa.ngxn@gmail.com>
2020-09-15 01:59:12 -0400

    fixed get_bars function

--- market_data.py
+++ market_data.py
@@ -8,18 +8,19 @@ from dateutil.relativedelta import relativedelta
 api = trade_api.REST(api_key_id, secret_key, base_url='https://paper-api.alpaca.markets/')
 
 
-def get_bars(symbols="", time_num=0, time_type="years"):
+def get_bars(symbols, day_count=0, week_count=0, month_count=0, year_count=0):
     while True:
         current_raw = datetime.datetime.now()
-        start_raw = (current_raw - relativedelta(days=7))
+        start_raw = (current_raw - relativedelta(days=day_count, weeks=week_count, months=month_count, years=year_count))
         start_round = pd.to_datetime(start_raw).round('1s')
         start = start_round.isoformat() + '-04:00'
-        barset = api.get_barset('AAPL', 'day', after=start)
+        barset = api.get_barset(symbols, 'day', after=start)
         aapl_bars = barset['AAPL']
 
         print(aapl_bars)
 
-        time.sleep(1)
+        time.sleep(100000)
 
 
-get_bars()
+my_symbols = 'AAPL'
+get_bars(symbols=my_symbols, day_count=2)
