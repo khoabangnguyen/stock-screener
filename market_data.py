3d7dc2eb33c4f46957dc3cee02767f97469a6367
Khoa <khoa.ngxn@gmail.com>
2020-09-19 00:13:31 -0400

    Added time, try to group helpers

--- market_data.py
+++ market_data.py
@@ -40,17 +40,41 @@ def get_volumes(symbols, barset):
             volumes.append(x[-1].v)
     return volumes
 
+def get_times(symbols, barset):
+    times = []
+    for symbol in symbols:
+        x = barset[symbol]
+        if len(x) == 0:
+            times.append('None')
+        else:
+            times.append(x[-1].t)
+    return times
+
+def get_change(symbols, barset):
+    percentage_changes = []
+    for symbol in symbols:
+        x = barset[symbol]
+        if len(x) == 0:
+            percentage_changes.append('None')
+        else:
+            end = x[0].c
+            start = x[0].o
+            percentage_changes.append("{:.2%}".format((end - start)/start))
+    return percentage_changes
+
 
 def create_df(symbols):
     bars = {}
     symbols_subs = [symbols[x:x + 100] for x in range(0, len(symbols), 100)]
     for sub in symbols_subs:
-        barset = get_bars(sub, day_count=1)
+        barset = get_bars(sub, day_count=7)
         for i in barset:
             bars[i] = barset[i]
     closing_prices = get_current_prices(symbols, bars)
+    percent_changes = get_change(symbols, bars)
     volumes = get_volumes(symbols, bars)
-    data = {'Name': symbols, 'Sector': sectors, 'Price': closing_prices, 'Volumes': volumes}
+    times = get_times(symbols, bars)
+    data = {'Name': symbols, 'Sector': sectors, 'Time': times, 'Price': closing_prices, '% Change': percent_changes, 'Volumes': volumes}
     df = pd.DataFrame(data)
     df.to_csv(r'export_dataframe.csv', index=False, header=True)
     return df
