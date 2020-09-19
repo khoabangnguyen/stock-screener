4235cc0085ca777c05df94318e05865f89b30f79
Khoa <khoa.ngxn@gmail.com>
2020-09-18 21:36:13 -0400

    Added S&P 500 Added export to excel Need to lift api call out of get_current_prices

--- market_data.py
+++ market_data.py
@@ -4,28 +4,44 @@ import pandas as pd
 import datetime
 import time
 from dateutil.relativedelta import relativedelta
+from get_s_p_500 import symbols as sp500_symbols
 
 api = trade_api.REST(api_key_id, secret_key, base_url='https://paper-api.alpaca.markets/')
 
 
-def get_bars(symbols, day_count=0, week_count=0, month_count=0, year_count=0):
+def get_bars(symbols, time_frame='day', day_count=0, week_count=0, month_count=0, year_count=0):
     while True:
         current_raw = datetime.datetime.now()
         start_raw = (current_raw - relativedelta(days=day_count, weeks=week_count, months=month_count, years=year_count))
         start_round = pd.to_datetime(start_raw).round('1s')
         start = start_round.isoformat() + '-04:00'
-        barset = api.get_barset(','.join(symbols), 'day', after=start)
+        barset = api.get_barset(','.join(symbols), time_frame, after=start)
 
-        closing_prices = []
-        for symbol in symbols:
-            closing_prices.append(barset[symbol][-1].c)
-            #print(barset[symbol][0].__dict__['_raw'])
-        data = {'name': symbols, 'price': closing_prices}
-        df_marks = pd.DataFrame(data)
-        print(df_marks)
-
-        time.sleep(100000)
+        return barset
 
 
-my_symbols = ['AAPL', 'AMZN']
-get_bars(symbols=my_symbols, day_count=10)
+def get_current_prices(symbols):
+    closing_prices = []
+    symbols_subs = [symbols[x:x+100] for x in range(0, len(symbols), 100)]
+    count = 0
+    for sub in symbols_subs:
+        barset = get_bars(sub, day_count=1)
+        for symbol in sub:
+            x = barset[symbol]
+            if len(x) == 0:
+                #print(symbol)
+                closing_prices.append('None')
+            else:
+                closing_prices.append(x[-1].c)
+    return closing_prices
+
+
+def create_df(symbols):
+    data = {'Name': symbols, 'Price': get_current_prices(symbols)}
+    df = pd.DataFrame(data)
+    df.to_csv(r'export_dataframe.csv', index=False, header=True)
+    return df
+
+
+print(create_df(sp500_symbols))
+
